5eab2bfab42f9bb7cbf7087f2a8e5b83d5da5354
QUYNH NHU <quynhnhu@QUYNHs-MacBook-Pro.local>
2024-05-16 23:37:02 -0700

    component create1

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import logo from "./logo.svg";
 import "./App.css";
-
+import Product from "./components/Product";
 // // import tất cả
 // import * as constantAll from "./constants/constant";
 
@@ -9,15 +9,16 @@ import "./App.css";
 // console.log(constantAll.name);
 
 // Dùng DESTRUCTURING
-import { name, job } from "./constants/constant";
-console.log(name);
-// Lấy biến name
-console.log(job);
+// import { name, job } from "./constants/constant";
+// console.log(name);
+// // Lấy biến name
+// console.log(job);
 
 function App() {
   return (
     <div className="App">
       <h1>Hello World</h1>
+      <Product />
     </div>
   );
 }
