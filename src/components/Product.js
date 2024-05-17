7170ccaa74f5e8f2a764982b0799e6c5fee5effd
QUYNH NHU <quynhnhu@QUYNHs-MacBook-Pro.local>
2024-05-17 01:15:05 -0700

    component create

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,4 +1,10 @@
+import React from "react";
 function Product() {
-  return <h1>Product</h1>;
+  return (
+    <>
+      <h1>Product</h1>
+      <button>Create product</button>
+    </>
+  );
 }
 export default Product;
