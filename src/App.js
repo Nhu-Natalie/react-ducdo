5b7a081dc6c08fc6383d5177de06dda34ff55255
Nhu-Natalie <natalie.bui@patroids.com>
2024-05-19 23:02:13 -0700

    event reactjs

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from "./logo.svg";
+import { useState } from "react";
 import "./App.css";
 
-// // import tất cả
-// import * as constantAll from "./constants/constant";
-
-// console.log(constantAll);
-// // Lấy biến name
-// console.log(constantAll.name);
-
-// Dùng DESTRUCTURING
-import { name, job } from "./constants/constant";
-console.log(name);
-// Lấy biến name
-console.log(job);
-
 function App() {
+  const [counter, setCounter] = useState(1);
+
+  const handleIncrement = () => {
+    setCounter(counter + 1); //LÀM FUNCTION APP CHẠY LẠI
+  };
+
+  console.dir(handleIncrement);
+
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <h1>Counter {counter}</h1>
+      <button onClick={handleIncrement}>Increment</button>
     </div>
   );
 }
 
 export default App;
+
+//RUN APP
+//SAU MỖI LẦN CHẠY NÓ LUÔN NHỚ ĐC GIÁ TRỊ EVENT HANDLER TRƯỚC ĐÓ NHỜ CLOSURE
+//===================
+//Chay lan 1
+//function App đc chạy >> object [counter, setCounter] đc tạo trong RAM
+//Khi nhấn button increament >> function handleIncrement mới đc chạy
+//object [count, setCounter] đc gán lại
+
+// MỖI LẦN FUNCTION APP CHẠY NÓ SẼ TẠO RA 1 INSTANCE CLOSURE KHÁC NHAU HOÀN TOÀN
+
+// CLOSURE: function bên trong có thể lưu giữ giá trị của function bên ngoài
+
+//Muốn FUNCTION handleIncrement là 1 CLOSURE phải thoả mãn 2 điều kiện
+//===================================================================
+// 1. handleIncrement: phải là hàm đc bao đóng trong function
+// (handleIncrement đã đc bao đóng trong function App)
+
+// 2. Nó phải sử dụng biến của function bên ngoài
+// ()
