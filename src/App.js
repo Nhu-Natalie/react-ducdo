5323a03950fa5ebb698ed4be3cc0034c08396d19
Nhu-Natalie <natalie.bui@patroids.com>
2024-05-19 22:05:49 -0700

    arrow_function_1

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
+import { useState } from "react";
 import "./App.css";
 
 function App() {
+  const [tags, setStags] = useState(["tag1", "tag2", "tag3"]);
   return (
     <div className="App">
       <h1>Hello World</h1>
+      <ul>
+        {tags.map((tag) => {
+          return <li key={tag}>{tag}</li>;
+        })}
+      </ul>
     </div>
   );
 }
