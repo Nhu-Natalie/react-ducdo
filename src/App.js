4ef7715ff05c4e13f6ca5eb39704ef68e5e6e705
Nhu-Natalie <natalie.bui@patroids.com>
2024-05-24 01:49:23 -0700

    useEffect with dependency

--- src/App.js
+++ src/App.js
@@ -1,16 +1,32 @@
 import { useEffect, useState } from "react";
 import "./App.css";
 
+let i = 0;
+
 function App() {
+  i++;
+  console.log(
+    "%crender start version 00" + i,
+    "background:#222; color:#bada55"
+  );
+
+  const id = 1; // id đc sử dụng trong useEffect nên nó cần đc update cũng như theo dõi trong callback function
+
   const [post, setPost] = useState({});
+  console.log(`id version 00${i}`, id);
+  console.log("post", post);
   useEffect(() => {
-    console.log("useEffect running");
-    fetch("https://jsonplaceholder.typicode.com/todos/1")
+    // Do callback có sử dụng id bên ngoài nên phải khai báo theo dõi id trong mảng tham số thứ 2 của useEffect
+    console.log("useEffect running version 00" + i);
+    fetch("https://jsonplaceholder.typicode.com/todos/" + id)
       .then((response) => response.json())
       .then((json) => setPost(json));
-  }, []);
+  }, [id]);
 
   // Dependency []: chỉ chạy duy nhất 1 lần Component đc mounted
+  // []: Ko cần theo dõi hay update gì cả
+
+  // [id]: so sánh id giữa các version nếu khác useEffect sẽ đc chạy
 
   return (
     <div className="App">
@@ -18,12 +34,12 @@ function App() {
         <h1>{post.id}</h1>
         <p>{post.title}</p>
       </div>
+      {console.log("render end version 00" + i)}
     </div>
   );
 }
 
 export default App;
-
 // 1. Tại sao lại bị hiện tượng call apis liên tục >> vòng lặp vô tận
 
 // Lần đầu render App version 001
@@ -56,7 +72,7 @@ export default App;
 //   </div>
 // );
 
-// useEffect(() => {
+/// useEffect(() => {
 //   fetch("https://jsonplaceholder.typicode.com/todos/1")
 //     .then((response) => response.json())
 //     .then((json) => setPost(json));
