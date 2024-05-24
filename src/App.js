fd6b2ffb74da434c711ebc41a2778730dc55bbcd
Nhu-Natalie <natalie.bui@patroids.com>
2024-05-23 23:40:17 -0700

    call api

--- src/App.js
+++ src/App.js
@@ -1,25 +1,67 @@
-import logo from "./logo.svg";
+import { useEffect, useState } from "react";
 import "./App.css";
 
-// // import tất cả
-// import * as constantAll from "./constants/constant";
-
-// console.log(constantAll);
-// // Lấy biến name
-// console.log(constantAll.name);
-
-// Dùng DESTRUCTURING
-import { name, job } from "./constants/constant";
-console.log(name);
-// Lấy biến name
-console.log(job);
-
 function App() {
+  const [post, setPost] = useState({});
+  useEffect(() => {
+    console.log("useEffect running");
+    fetch("https://jsonplaceholder.typicode.com/todos/1")
+      .then((response) => response.json())
+      .then((json) => setPost(json));
+  }, []);
+
+  // Dependency []: chỉ chạy duy nhất 1 lần Component đc mounted
+
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <div>
+        <h1>{post.id}</h1>
+        <p>{post.title}</p>
+      </div>
     </div>
   );
 }
 
 export default App;
+
+// 1. Tại sao lại bị hiện tượng call apis liên tục >> vòng lặp vô tận
+
+// Lần đầu render App version 001
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+// Component amounted >> post chưa có data >> data rỗng {} nên ko show gì
+// return (
+//   <div className="App">
+//     <div>
+//       <h1>{post.id}</h1>
+//       <p>{post.title}</p>
+//     </div>
+//   </div>
+// );
+
+// useEffect(() => {
+//   fetch("https://jsonplaceholder.typicode.com/todos/1")
+//     .then((response) => response.json())
+//     .then((json) => setPost(json)); // state thay đổi {} => {id:1, ....} >> Component re-render
+// });
+
+// Lần re-render 1 App version 002
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+// Component re-render >> post đã có data >> show data
+// return (
+//   <div className="App">
+//     <div>
+//       <h1>{post.id}</h1>
+//       <p>{post.title}</p>
+//     </div>
+//   </div>
+// );
+
+// useEffect(() => {
+//   fetch("https://jsonplaceholder.typicode.com/todos/1")
+//     .then((response) => response.json())
+//     .then((json) => setPost(json));
+//     STATE thay đổi do json là 2 OBJECTs khác nhau mỗi lần call APIs
+//     nhưng chúng có giá trị giống nhau {id:1, ....} => {id:1, ....} >>
+//     Component lại re-render
+//     >> Vòng lặp vô tận xảy ra
+// });
